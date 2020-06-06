9beb6da79ec406905dd8b7b75244c7c657cd0b28
paulweichhart <paul.weichhart@me.com>
2020-06-06 14:41:43 +0200

    Initial commit

---
+++ Portfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Portfolio
+//
+//  Created by Paul Weichhart on 06.06.20.
+//  Copyright © 2020 Paul Weichhart. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
